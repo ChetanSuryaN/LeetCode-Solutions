b0837a861bc2228185c7bc2215f6b9b87775cf20
ChetanSuryaN <chetansurya140@gmail.com>
2026-08-24 09:16:29 +0530

    Time: 6 ms (52.43%), Space: 42.5 MB (99.57%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int longestPalindrome(String s) 
+    {
+        boolean hascentre=false;
+       int[] c=new int[26];
+        int[] ca =new int[26];
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(Character.isLowerCase(s.charAt(i)))
+            c[s.charAt(i)-'a']++;
+            else
+            ca[s.charAt(i)-'A']++;
+        }
+        Arrays.sort(c);
+        Arrays.sort(ca);
+        for(int i=25;i>=0;i--)
+        {
+            if(c[i]%2==0)
+            {
+                ans+=c[i];
+            }
+            else
+            {
+                ans+=c[i]-1;
+                hascentre=true;
+            }
+            if(ca[i]%2==0)
+            {
+                ans+=ca[i];
+            }
+            else
+            {
+                ans+=ca[i]-1;
+                hascentre=true;
+            }
+        }
+        return hascentre?ans+1:ans;
+        
+    }
+}
