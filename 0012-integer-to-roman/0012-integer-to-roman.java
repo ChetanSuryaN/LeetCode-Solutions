dc3fa3b7f2fe600fc1df3af72e4c019d9e84b3fe
ChetanSuryaN <chetansurya140@gmail.com>
2026-08-27 13:56:54 +0530

    Time: 9 ms (5.59%), Space: 48.1 MB (5.16%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String intToRoman(int num) 
+    {
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(0,"");
+        map.put(1,"I");
+        map.put(2,"II");
+        map.put(3,"III");
+        map.put(4,"IV");
+        map.put(5,"V");
+        map.put(6,"VI");
+        map.put(7,"VII");
+        map.put(8,"VIII");
+        map.put(9,"IX");
+        map.put(10,"X");
+        map.put(20,"XX");
+        map.put(30,"XXX");
+        map.put(40,"XL");
+        map.put(50,"L");
+        map.put(60,"LX");
+        map.put(70,"LXX");
+        map.put(80,"LXXX");
+        map.put(90,"XC");
+        map.put(100,"C");
+        map.put(200,"CC");
+        map.put(300,"CCC");
+        map.put(400,"CD");
+        map.put(500,"D");
+        map.put(600,"DC");
+        map.put(700,"DCC");
+        map.put(800,"DCCC");
+        map.put(900,"CM");
+        map.put(1000,"M");
+        map.put(2000,"MM");
+        map.put(3000,"MMM");
+        String input=String.valueOf(num);
+        String ans="";
+        for(int i=0;i<input.length();i++)
+        {
+            int temp=input.charAt(i)-'0';
+            temp=temp*(int)Math.pow(10.0,(input.length()-i-1));
+            ans+=map.get(temp);
+        }
+        return ans;
+    }
+}
