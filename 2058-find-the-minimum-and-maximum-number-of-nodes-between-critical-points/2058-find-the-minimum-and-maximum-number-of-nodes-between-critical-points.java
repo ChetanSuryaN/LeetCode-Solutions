9c5dd35bc4de852e1b1add64f4d50ac65d7ee6de
ChetanSuryaN <chetansurya140@gmail.com>
2026-08-31 09:33:05 +0530

    Time: 4 ms (99.87%), Space: 105.9 MB (94.26%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -11,42 +11,45 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) 
     {
-        List<Integer> list=new ArrayList<>();
         
         ListNode curr=head.next;
         int prev=head.val;
-        int max=-1;
+        
         int i=1;
-        int min=0;
+        
+        int firstindex=-1;
+        int previndex=-1;
+         int mindis=Integer.MAX_VALUE;        
         while(curr!=null&&curr.next!=null)
         {
-            if(curr.val>prev&&curr.val>curr.next.val)
+            if((curr.val>prev&&curr.val>curr.next.val)||(curr.val<prev&&curr.val<curr.next.val))
             {
-               list.add(i);
+                if(firstindex==-1)
+                {
+                    firstindex=i;
+                }  
+                else
+                {
+                    mindis=Math.min(mindis,i-previndex);
+                }
+                previndex=i;             
             }
-            else if(curr.val<prev&&curr.val<curr.next.val)
-            {
-                list.add(i);
-            }
+           
+            
             prev=curr.val;
             i++;
             curr=curr.next;
         }
-       int arr[]=new int[2];
-       if(list.size()<2)
-       {
-         arr[0]=-1;
-         arr[1]=-1;
-         return arr;
-       }
-       int mindis=Integer.MAX_VALUE;
-       for(int k=0;k<list.size()-1;k++)
-       {
-        mindis=Math.min(list.get(k+1)-list.get(k),mindis);         
-       }
-       arr[1]=list.get(list.size()-1)-list.get(0);
-       arr[0]=mindis;
-       return arr;
-
+          int arr[]=new int[2];
+        if(previndex==-1||previndex==firstindex)
+        {
+            arr[0]=-1;
+            arr[1]=-1;
+            return arr;
+        }
+    
+      arr[0]=mindis;
+      arr[1]=previndex-firstindex;
+      return arr;
     }
 }
